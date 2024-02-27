e28507dbc1a08f55cd49979a7958a8e209b5a55b
Martyna <martyna.wekwerth@protonmail.com>
2024-02-27 15:34:20 +0100

    added a slider to projects

--- js/main.js
+++ js/main.js
@@ -6,3 +6,53 @@ const navBtn = document.querySelector('.burger-btn');
     }
 
     navBtn.addEventListener('click', handleNav);
+
+let items = document.querySelectorAll('.slider-wrapper .list .item');
+let next = document.getElementById('next');
+let prev = document.getElementById('prev');
+let thumbnails = document.querySelectorAll('.thumbnail .item');
+
+let countItem = items.length;
+let itemActive = 0;
+
+next.onclick = function() {
+    itemActive = itemActive + 1;
+    if(itemActive >= countItem){
+        itemActive = 0;
+    }
+    showSlider();
+}
+
+prev.onclick = function() {
+    itemActive = itemActive -1;
+    if(itemActive < 0) {
+        itemActive = countItem -1;
+    }
+    showSlider();
+}
+
+let refreshInterval = setInterval(() => {
+    next.click();
+}, 5000)
+
+function showSlider() {
+    let itemActiveOld = document.querySelector('.slider-wrapper .list .active');
+    let thumbnailActiveOld = document.querySelector('.thumbnail .active');
+    itemActiveOld.classList.remove('active');
+    thumbnailActiveOld.classList.remove('active');
+
+    items[itemActive].classList.add('active');
+    thumbnails[itemActive].classList.add('active');
+
+    clearInterval(refreshInterval);
+    refreshInterval = setInterval(() => {
+        next.click();
+    }, 5000)
+} 
+
+thumbnails.forEach((thumbnail, index) => {
+    thumbnail.addEventListener('click', () => {
+        itemActive = index;
+        showSlider();
+    })
+})
