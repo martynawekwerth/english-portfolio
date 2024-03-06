c38faf985e7a0f7c4168c8f464c5bc6f98c88f7c
Martyna <martyna.wekwerth@protonmail.com>
2024-03-06 15:53:09 +0100

    added buttons to newly created task

--- js/toDoList.js
+++ js/toDoList.js
@@ -40,20 +40,43 @@ const addNewTask = () => {
         $idNumber++;
         $newTask = document.createElement('li');
         $newTask.innerText = $todoInput.value;
-        $newTask.setAttribute('id', 'todo-${$idNumber}');
+        $newTask.setAttribute('id', `todo-${$idNumber}`);
         $ulList.appendChild($newTask);
 
         $todoInput.value = '';
         $alertInfo.innerText = '';
+        createToolsArea();
     } else {
         $alertInfo.innerText = 'Enter task value!';
     }
 }
 
 const enterCheck = () => {
-    if (event.key === 13) {
+    if (event.key === 'Enter')  { 
         addNewTask();
     }
+}
+
+const createToolsArea = () => {
+    const toolsPanel = document.createElement('div');
+    toolsPanel.classList.add('tools');
+    $newTask.appendChild(toolsPanel);
+
+    const completeBtn = document.createElement('button');
+    completeBtn.classList.add('complete');
+    completeBtn.innerHTML = '<i class="fas fa-check"></i>';
+
+    const editBtn = document.createElement('button');
+    editBtn.classList.add('edit');
+    editBtn.innerHTML = 'EDIT';
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.classList.add('delete');
+    deleteBtn.innerHTML = '<i class="fas fa-times"></i>';
+
+    toolsPanel.appendChild(completeBtn);
+    toolsPanel.appendChild(editBtn);
+    toolsPanel.appendChild(deleteBtn);
 }
 
 document.addEventListener('DOMContentLoaded', main);
